4f7a0706da3344836320130b3a54b73b178ea6a7
fxy314 <89053574+fxy314@users.noreply.github.com>
2021-08-28 23:30:46 +0800

    Update shape.cpp

--- shape.cpp
+++ shape.cpp
@@ -53,7 +53,7 @@ void shape::use_inch()
  * @return double 面积的数值
  */
 double shape::get_area()
-{   
+{
     area = cut_to_3(area);
     return area;
 }
@@ -121,7 +121,7 @@ square::square():shape()
  * @param input_len 正方形的边长
  */
 void square::set_param(double input_len)
-{   
+{
     if((input_len - 0 > MINVALUE) && (input_len < MAXVALUE))
     {
         length = cut_to_3(input_len);
@@ -166,14 +166,23 @@ void square::use_centi()
 }
 
 /*——————————————————————————以下为rectangle类————————————————————————————*/
+/**
+ * @brief Construct a new rectangle::rectangle object
+ * @details 继承于shape类的构造函数，只是将type的值更改了
+ */
 rectangle::rectangle():shape()
 {
     type = "图形种类为：长方形";
 }
-
+/**
+ * @brief 设置长方形参数
+ * @details 通过输入确定长方形的长和宽，同时修改长方形的面积，均只保留三位小数
+ * @exception 长度参数必须在设置范围内，即MINVALUE到MAXVALUE之间，否则抛出const char[]异常 ："输入参数错误！"
+ * @param input_len 长方形的长 input_hei 长方形的宽
+ */
 void rectangle::set_param(double input_len, double input_hei)
-{   
-    if( (input_len - 0 > MINVALUE ) && (input_hei - 0 > MINVALUE) 
+{
+    if( (input_len - 0 > MINVALUE ) && (input_hei - 0 > MINVALUE)
     && (input_len < MAXVALUE) && (input_hei < MAXVALUE))
     {
         length = cut_to_3(input_len);
@@ -186,40 +195,63 @@ void rectangle::set_param(double input_len, double input_hei)
         throw "输入参数错误！";
     }
 };
-
+/**
+ * @brief 用于长方形对象的use_centi()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改长和宽的功能，长方形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从英寸变为厘米时使用，如果已经选择厘米，再次选择厘米时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void rectangle::use_centi()
 {
     shape::use_centi();
     length = cut_to_3(length * INCH);
     height = cut_to_3(height * INCH);
 }
-
+/**
+ * @brief 用于长方形对象的use_inch()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改长和宽的功能，长方形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从厘米变为英寸时使用，如果已经选择英寸，再次选择英寸时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void rectangle::use_inch()
 {
     shape::use_inch();
     length = cut_to_3(length / INCH);
     height = cut_to_3(height / INCH);
 }
-
+/**
+ * @brief 返回以厘米为单位的长方形的长，保留三位小数
+ * @return double 长方形的长
+ */
 double rectangle::get_length()
 {
     return length;
 }
-
+/**
+ * @brief 返回以厘米为单位的长方形的宽，保留三位小数
+ * @return double 长方形的宽
+ */
 double rectangle::get_height()
 {
     return height;
 }
 
 /*————————————————————————————以下为triangle类————————————————————————————*/
+/**
+ * @brief Construct a new triangle::triangle object
+ * @details 继承于shape类的构造函数，只是将type的值更改了
+ */
 triangle::triangle():shape()
 {
     type = "图形种类为：三角形";
 }
-
+/**
+ * @brief 设置三角形参数
+ * @details 通过输入确定三角形的底边和该底边上的高，同时修改三角形的面积，均只保留三位小数
+ * @exception 长度参数必须在设置范围内，即MINVALUE到MAXVALUE之间，否则抛出const char[]异常 ："输入参数错误！"
+ * @param input_len 三角形的底边长 input_hei 三角形该底边上的高
+ */
 void triangle::set_param(double input_len, double input_hei)
 {
-    
+
     if( (input_len - 0 > MINVALUE ) && (input_hei - 0 > MINVALUE)
     && (input_len < MAXVALUE) && (input_hei < MAXVALUE))
     {
@@ -233,39 +265,62 @@ void triangle::set_param(double input_len, double input_hei)
         throw "输入参数错误！";
     }
 }
-
+/**
+ * @brief 用于三角形对象的use_centi()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改底边和该底边上的高的功能，三角形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从英寸变为厘米时使用，如果已经选择厘米，再次选择厘米时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void triangle::use_centi()
 {
     shape::use_centi();
     length = cut_to_3(length * INCH);
     height = cut_to_3(height * INCH);
 }
-
+/**
+ * @brief 用于三角形对象的use_inch()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改底边和该底边上的高的功能，三角形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从厘米变为英寸时使用，如果已经选择英寸，再次选择英寸时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void triangle::use_inch()
 {
     shape::use_inch();
     length = cut_to_3(length / INCH);
     height = cut_to_3(height / INCH);
 }
-
+/**
+ * @brief 返回以厘米为单位的三角形的底边长，保留三位小数
+ * @return double 三角形的底边长
+ */
 double triangle::get_length()
 {
     return length;
 }
-
+/**
+ * @brief 返回以厘米为单位的三角形的该底边上的高，保留三位小数
+ * @return double 三角形的该底边上的高
+ */
 double triangle::get_height()
 {
     return height;
 }
 
 /*————————————————————————————————以下为circile类——————————————————————————————————*/
+/**
+ * @brief Construct a new circle::circle object
+ * @details 继承于shape类的构造函数，只是将type的值更改了
+ */
 circle::circle():shape()
 {
     type = "图形种类为：圆形";
 }
-
+/**
+ * @brief 设置圆形参数
+ * @details 通过输入确定圆形的直径，同时修改圆形的面积，均只保留三位小数
+ * @exception 长度参数必须在设置范围内，即MINVALUE到MAXVALUE之间，否则抛出const char[]异常 ："输入参数错误！"
+ * @param input_d 圆形的直径
+ */
 void circle::set_param(double input_d)
-{   
+{
     if((input_d - 0 > MINVALUE) && (input_d < MAXVALUE))
     {
         diameter = cut_to_3(input_d);
@@ -277,22 +332,32 @@ void circle::set_param(double input_d)
         throw "输入参数错误！";
     }
 }
-
+/**
+ * @brief 用于圆形对象的use_centi()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改圆形的直径的功能，圆形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从英寸变为厘米时使用，如果已经选择厘米，再次选择厘米时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void circle::use_centi()
 {
     shape::use_centi();
     diameter = cut_to_3(diameter * INCH);
 }
-
+/**
+ * @brief 用于圆形对象的use_inch()函数
+ * @details 该函数继承了shape中原函数更改面积的特点，增加了更改圆形的直径的功能，圆形对象应该调用该函数而不是shape类中的原函数
+ * @note 该函数只能在单位从厘米变为英寸时使用，如果已经选择英寸，再次选择英寸时不能调用，而且不要在厘米与英寸之间反复切换
+ */
 void circle::use_inch()
 {
     shape::use_inch();
     diameter = cut_to_3(diameter / INCH);
 }
-
+/**
+ * @brief 返回以厘米为单位的圆形的直径，保留三位小数
+ * @return double 圆形的直径
+ */
 double circle::get_diameter()
 {
     return diameter;
 }
-
 
